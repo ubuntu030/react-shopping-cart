f881d76f59c7a9dea922edd5539e46369d3475a9
wikisiyun <wikisiyun@gmail.com>
2019-05-17 14:29:13 +0800

    - 顯示商品資訊

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -127,7 +127,7 @@ class Shop extends React.Component {
             {/* End hero unit */}
             <Grid container spacing={40}>
               {/* TODO: loding 效果 */}
-              <CardGenerate classes={classes} />
+              <CardGenerate products={this.state.products} classes={classes} />
             </Grid>
           </div>
         </main>
@@ -155,22 +155,22 @@ Shop.propTypes = {
  * @param {Object} props 
  */
 function CardGenerate(props) {
-  const { classes } = props;
+  const { products, classes } = props;
   return (
-    cards.map(card => (
-      <Grid item key={card} xs={12} sm={6} md={4} lg={3}>
+    products.map(item => (
+      <Grid item key={item.id} xs={12} sm={6} md={4} lg={3}>
         <Card className={classes.card}>
           <CardMedia
             className={classes.cardMedia}
-            image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
-            title="Image title"
+            image={item.img}
+            title={item.alt_description}
           />
           <CardContent className={classes.cardContent}>
             <Typography gutterBottom variant="h5" component="h2">
-              商品名稱
+              {item.name}
             </Typography>
             <Typography>
-              一些關於商品的描述?
+              {item.alt_description}
             </Typography>
           </CardContent>
           <CardActions>
@@ -189,6 +189,7 @@ function CardGenerate(props) {
 
 /**
  * 取得照片
+ * @return {Promise}
  */
 function fetchPhoto() {
   return Unsplash.photos.getRandomPhoto({ count: 12, query: 'nature', width: '800', height: '600' })
@@ -205,12 +206,13 @@ function fetchPhoto() {
 /**
  * 將需要的資料整理成一綑
  * @param {Object} photoData fetchPhotos 回來的資料 
+ * @return {Array<Object>} 整理後的照片資料
  */
 function formatPhotosData(photoData) {
   const data = [...photoData];
   const nData = data.map(data => {
-    const { id, exif, alt_description, urls: { regular: img } } = data;
-    return { id, img, exif, alt_description }
+    const { id, exif, alt_description, urls: { regular: img }, user: { name } } = data;
+    return { id, name, img, exif, alt_description }
   });
   console.log('[formatPhotos] ', nData);
   return nData;

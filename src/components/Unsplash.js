d03e8813f17a5379c2b434cea0db2e2b057565d6
wikisiyun <wikisiyun@gmail.com>
2019-05-17 11:10:56 +0800

    - Unsplash 模組

---
+++ src/components/Unsplash.js
@@ -0,0 +1,8 @@
+import Unsplash from 'unsplash-js';
+
+const unsplash = new Unsplash({
+    applicationId: '39181425fbdbae9517bc80c58841ac99e98425d8581a7976e388114f8e47f6db',
+    secret: '9062c5984da93f36b712d7d2bdefa6ebed9b1ce678717dba097cbc4e333c4756'
+});
+
+export default unsplash;
